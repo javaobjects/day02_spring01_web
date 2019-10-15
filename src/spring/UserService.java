aa35a2cf4c8856e7290f0022ce0a0c05009feeb5
yanbo0039 <yanbo0039@yeah.net>
2019-10-15 09:06:39 +0800

    更新注释

--- src/spring/UserService.java
+++ src/spring/UserService.java
@@ -1,6 +1,19 @@
 package spring;
+
 /**
- * 使用spring的时候，UserService依赖于IUserDao，将new对象的权利管理在applicationContext.xml中，控制权在spring的手里，即new对象的权利从自己手中反转给了spring（IOC\DI）
+ * 
+* <p>Title: UserService</p>  
+* <p>
+*	Description: 
+* 	使用spring的时候，
+* 	UserService依赖于IUserDao，
+* 	将new对象的权利管理在applicationContext.xml中，
+* 	控制权在spring的手里，
+* 	即new对象的权利从自己手中反转给了spring（IOC\DI）
+*   
+* </p> 
+* @author xianxian 
+* @date 2019年10月14日
  */
 public class UserService {
 
